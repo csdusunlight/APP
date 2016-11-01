4d1700a61c387c78933a3461733be7ebf2f72fe0
lch <18500581509@163.com>
2016-11-01 15:00:13 +0800

    gai

--- js/share.js
+++ js/share.js
@@ -1,24 +1,105 @@
-var body = document.getElementsByTagName("body")[0],
-	share = document.getElementById("share");
-//		动态创建HTML
-var div = document.createElement('div');
-var ua = navigator.userAgent.toLowerCase();
-if(ua.match(/MicroMessenger/i)=="micromessenger") {
-    div.className = 'share-guide-box share-top';
-} else if (ua.indexOf('qq/')!= -1){
-    div.className = 'share-guide-box share-top';
-} else {
-    div.className = 'share-guide-box share-bottom';
-}
-	div.id = 'share_guide_box';
-	body.appendChild(div);
-
-//		点击分享按钮弹出弹窗
-var share_guide_box = document.getElementById("share_guide_box");
-share.addEventListener("tap",function () {
-	share_guide_box.style.display = "block";
-});
-//		点击背景关闭分享弹窗;
-share_guide_box.addEventListener("tap",function () {
-	share_guide_box.style.display = "none";
-});
+(function(owner) {
+	var Intent = null;
+	var main = null;
+	var Share = {
+		shares: {},
+		init: function() {
+			if (mui.os.plus && mui.os.android) {
+				mui.plusReady(function() {
+					Intent = plus.android.importClass("android.content.Intent");
+					main = plus.android.runtimeMainActivity();
+					this.supportShare() && plus.share.getServices(function(services) {
+						this.shares = {};
+						mui.each(services, function(index, service) {
+							this.shares[service.id] = service;
+						}.bind(this));
+					}.bind(this), function(e) {
+						console.log("获取分享服务列表失败：" + e.message);
+					});
+				}.bind(this));
+			}
+		},
+		supportShare: function() {
+			var versions = plus.runtime.innerVersion.split('.');
+			return versions[versions.length - 1] >= 20133;
+		},
+		open: function(message, callback) {
+			var self = this;
+			if (this.supportShare() && this.shares.weixin) { //高版本，可以使用plus share
+				plus.nativeUI.actionSheet({
+					title: '分享到',
+					cancel: "取消",
+					buttons: [{
+						title: "微信消息"
+					}, {
+						title: "微信朋友圈"
+					}, {
+						title: "更多分享"
+					}]
+				}, function(e) {
+					var index = e.index;
+					switch (index) {
+						case 1: //分享到微信好友
+							self.share('weixin', mui.extend(true, {}, message, {
+								extra: {
+									scene: 'WXSceneSession'
+								}
+							}), callback);
+							break;
+						case 2: //分享到微信朋友圈
+							message.title = message.content;
+							self.share('weixin', mui.extend(true, {}, message, {
+								extra: {
+									scene: 'WXSceneTimeline'
+								}
+							}), callback);
+							break;
+						case 3: //更多分享
+							self.openSysShare(message, callback);
+							break;
+					}
+				});
+			} else { //低版本，系统分享
+				self.openSysShare(message, callback);
+			}
+		},
+		share: function(id, message, callback) {
+			var self = this;
+			var service = self.shares[id];
+			if (!service) {
+				plus.nativeUI.alert('无效的分享服务[' + id + ']');
+				callback && callback(false);
+				return;
+			}
+			if (service.authenticated) {
+				self._share(service, message, callback);
+			} else {
+				service.authorize(function() {
+					self._share(service, message, callback);
+				}, function(e) {
+					plus.nativeUI.alert("认证授权失败");
+					callback && callback(false);
+				});
+			}
+		},
+		_share: function(service, message, callback) {
+			service.send(message, function() {
+				plus.nativeUI.toast("分享到\"" + service.description + "\"成功！");
+				callback && callback(true);
+			}, function(e) {
+				plus.nativeUI.toast("分享到\"" + service.description + "\"失败！");
+				callback && callback(false);
+			});
+		},
+		openSysShare: function(message, callback) {
+			var intent = new Intent(Intent.ACTION_SEND);
+			intent.setType("text/plain");
+			intent.putExtra(Intent.EXTRA_TEXT, message.content);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			main.startActivity(Intent.createChooser(intent, "系统分享"));
+			callback && callback(true);
+		}
+	};
+	Share.init();
+	owner.share = Share;
+})(window);
